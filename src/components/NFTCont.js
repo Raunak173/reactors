4f6fc7cd22dea55528b23ebd6142b54e0c97c682
Raunak Agarwal <raunakagarwal173@gmail.com>
2022-11-06 12:44:47 +0530

    added nft buying option

--- src/components/NFTCont.js
+++ src/components/NFTCont.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const NFTCont = () => {
-  return <div>NFTCont</div>;
+  return (
+    <div>
+      <a href="https://reacters-marketplace.netlify.app/" target="_black">
+        <button className="bg-red-600 flex justify-center items-center p-5">
+          <p className="text-white text-xl">Buy Membership NFTs</p>
+        </button>
+      </a>
+    </div>
+  );
 };
 
 export default NFTCont;
